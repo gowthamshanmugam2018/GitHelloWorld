2a742e5d2c8999e356edf264aa42a7dda5568295
Gowtham Shanmugam <gowthamshanmugam2018@outlook.com>
2021-09-05 00:33:15 -0700

    second master click listener commit

--- app/src/main/java/com/sample/githelloworld/MainActivity.kt
+++ app/src/main/java/com/sample/githelloworld/MainActivity.kt
@@ -3,12 +3,17 @@ package com.sample.githelloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.e("GTM","onCreate api called")
+        findViewById<TextView>(R.id.sectv1).setOnClickListener{
+            Toast.makeText(this,"GTM second master commit",Toast.LENGTH_LONG).show()
+        }
     }
 
     override fun onDestroy() {
